a8b51839ffd6e9c34da75aa80bd5d1bb19443e68
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-11-13 09:01:08 +0000

    Adding a basic footer

--- tests/app/RootLayout.test.tsx
+++ tests/app/RootLayout.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import RootLayout from "../../app/layout";
+import { metadata } from "../../app/layout";
 
 describe("RootLayout", () => {
-  it("renders children correctly", () => {
+  it("should have correct metadata values", () => {
+    expect(metadata.title).toBe("PropertyPulse | Find The Perfect Rental");
+  });
+
+  it("should render children correctly", () => {
     render(
       <RootLayout>
         <div>Test Child</div>
---
+++ tests/components/Footer.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Footer from "@/components/Footer";
+import "@testing-library/jest-dom";
+import React from "react";
+
+jest.mock("next/image", () => {
+  const MockImage = (props: React.ImgHTMLAttributes<HTMLImageElement>) =>
+    React.createElement("img", props);
+
+  MockImage.displayName = "MockNextImage";
+
+  return MockImage;
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("Footer", () => {
+  it("should render footer image", () => {
+    render(<Footer />);
+
+    const logoImage = screen.getByAltText("Logo");
+    expect(logoImage).toBeInTheDocument();
+    expect(logoImage).toHaveAttribute("src");
+  });
+
+  it("should render the correct copyright text with current year", () => {
+    render(<Footer />);
+    const currentYear = new Date().getFullYear();
+    expect(
+      screen.getByText(`© ${currentYear} PropertyPulse. All rights reserved.`)
+    ).toBeInTheDocument();
+  });
+});
